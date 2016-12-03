92534a3309a1768589f004882b3d9f5f10be41d8
Marzana Swierska <fanka2191@gmail.com>
2016-12-03 02:00:31 +0100

    Selektory w jQuery

---
+++ scripts.js
@@ -0,0 +1,15 @@
+(function() {
+	$(document).ready(function($) {
+		
+		$('.grid.grid-12').css('color', '#FFD700').css('border', 'solid 10px #000000');
+
+		$('.nav a[href^="http"]').css('color', 'green').css('border', 'solid 10px #000000');
+		
+		$('input:radio, input:checkbox').not(':checked').addClass('taadaam');
+		
+		$('#text p:first-child').css('background', '#FFD700').css('border', 'solid 10px #000000');
+		
+		$('.pagination-item:not(span)').css('color', '#FFD700').css('border', 'solid 10px #000000');
+
+	});
+})();
